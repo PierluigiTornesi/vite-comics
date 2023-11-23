f8efd40ce9d1feecde127a8e0699fb9112ac9582
PierluigiTornesi <pierluigi.tornesi02@gmail.com>
2023-11-23 15:27:29 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script>
+  
+  import AppHeader from './components/AppHeader.vue';
+  import ProductList from './components/ProductList.vue';
+  import AppFooter from './components/AppFooter.vue';
+
+  export default{
+    components:{
+    AppHeader,
+    ProductList,
+    AppFooter,
+}
+  }
+</script>
+
+<template>
+  <h1>Titolo</h1>
+  <AppHeader />
+  <ProductList />
+  <AppFooter />
+</template>
+
+<style lang="scss">
+@use "./style/general.scss"
+</style>
---
+++ src/components/AppFooter.vue
@@ -0,0 +1,13 @@
+<script>
+    export default{
+        
+    }
+</script>
+
+<template>
+  <h2>footer</h2> 
+</template>
+
+<style  scoped lang="scss">
+
+</style>
