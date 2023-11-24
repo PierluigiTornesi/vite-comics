05aca5901cb3fc10b0d94ce38d45b616b2af88f7
PierluigiTornesi <pierluigi.tornesi02@gmail.com>
2023-11-24 15:59:48 +0100

    add: card fumetti

---
+++ src/components/AppCard.vue
@@ -0,0 +1,40 @@
+<script>
+    export default{
+        props:{
+            series: String,
+            thumb: String,
+            price: String,
+            type:String,
+        }
+    }
+</script>
+
+<template>
+    <div class="card">
+        <img :src="thumb" :alt="series">
+        <h3>{{ series }}</h3>
+    </div>
+</template>
+
+<style  scoped lang="scss">
+    @use "../style/partials/mixin" as *;
+    @use "../style/partials/variables" as *;
+        
+    .card {
+        height: 100%;
+        
+        img{
+            width: 100%;
+            aspect-ratio: 1;
+            object-fit: cover;
+            object-position: top;
+        }
+
+        h3{
+            text-align: center;
+            font-size: 1rem;
+        }
+    }
+    
+
+</style>
